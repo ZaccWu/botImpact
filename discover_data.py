a337e881498772294c96eb1219e866990ae4d8e0
Ziyue Wu <670122025@qq.com>
2023-04-18 11:08:36 +0800

    Update discover_data.py

--- discover_data.py
+++ discover_data.py
@@ -38,20 +38,34 @@ def check_homo(edge_idx, Str):
         followers_of_i = edge_idx[1, :][i == edge_idx[0, :]]    # 0-focal, 1-friend
         num_friends = len(followers_of_i)
         if num_friends == 0:
+            if bot_label[i] == 1:
+                bot_degree.append(0)
+            else:
+                human_degree.append(0)
             continue
+
         same_propor = stance_label[followers_of_i].eq(stance_label[i]).sum() / len(followers_of_i)
         if bot_label[i] == 1:
-            bot_degree.append(len(followers_of_i))
+            bot_degree.append(num_friends)
             bot_homo.append(same_propor.item())
         else:
-            human_degree.append(len(followers_of_i))
+            human_degree.append(num_friends)
             human_homo.append(same_propor.item())
     print("Analysis of "+Str)
     print("Num of friends: ", np.mean(bot_degree), np.mean(human_degree))
     print("Friends' homo:", np.mean(bot_homo), np.mean(human_homo))
 
-check_homo(edge_index[:,(0==edge_type)], "user's followers")
-check_homo(edge_index[:,(1==edge_type)], "user's following")
-check_homo(edge_index[:,(2==edge_type)], "user's mention")
-check_homo(edge_index[:,(3==edge_type)], "user's replying")
-check_homo(edge_index[:,(4==edge_type)], "user's quoting")
+edge_index_0, edge_index_1 = edge_index[:,(0==edge_type)], edge_index[:,(1==edge_type)]
+edge_index_2, edge_index_3, edge_index_4 = edge_index[:,(2==edge_type)], edge_index[:,(3==edge_type)], edge_index[:,(4==edge_type)]
+
+check_homo(edge_index_0, "user's followers")    # degree: 2.09, 40.58, homo: 0.606, 0.728
+check_homo(edge_index_1, "user's following")    # degree: 14.19, 50.14, homo: 0.471, 0.617
+check_homo(edge_index_2, "user's mention")      # degree: 5.71, 13.26, homo: 0.575, 0.729
+check_homo(torch.concat([edge_index_2[1,:].unsqueeze(0), edge_index_2[0,:].unsqueeze(0)], dim=0),
+           "user's mentioner")                  # degree: 0.17, 15.31, homo: 0.926, 0.888
+check_homo(edge_index_3, "user's replying")     # degree: 2.12, 29.20, homo: 0.880, 0.904
+check_homo(torch.concat([edge_index_3[1,:].unsqueeze(0), edge_index_3[0,:].unsqueeze(0)], dim=0),
+           "user's replyer")                    # degree: 1.63, 29.39, homo: 0.968, 0.942
+check_homo(edge_index_4, "user's quoting")      # degree: 1.13, 10.00, homo: 0.719, 0.897
+check_homo(torch.concat([edge_index_4[1,:].unsqueeze(0), edge_index_4[0,:].unsqueeze(0)], dim=0),
+           "user's quoter")                     # degree: 0.09, 10.39, homo: 1, 0.958
