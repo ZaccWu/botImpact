49c01134f66a881e8862c08f7ea071de45ce20ed
Ziyue Wu <670122025@qq.com>
2023-05-06 20:30:17 +0800

    Update exec.py

--- exec.py
+++ exec.py
@@ -34,25 +34,43 @@ class MaskEncoder(torch.nn.Module):
 
 
 class botDetect(torch.nn.Module):
-    def __init__(self, in_dim, h_dim1, h_dim2, out_dim1=1, out_dim2=1):
+    def __init__(self, in_dim, h_dim, out_dim=2):
         super(botDetect, self).__init__()
-        self.convB1 = GCNConv(in_dim, h_dim1)
-        self.convB2 = GCNConv(h_dim1, out_dim1)
-
-        self.convY1 = GCNConv(in_dim, h_dim2)
-        self.convY2 = GCNConv(h_dim2, out_dim2)
+        self.convB1 = GCNConv(in_dim, h_dim)
+        self.convB2 = GCNConv(h_dim, out_dim)
 
     def forward(self, x, edge_index):
         xB1 = F.relu(self.convB1(x, edge_index))
         xB1 = F.dropout(xB1, p=0.5, training=self.training)
         xB2 = self.convB2(xB1, edge_index)
+        return xB2
 
-        xY1 = F.relu(self.convY1(x, edge_index))
-        xY1 = F.dropout(xY1, p=0.5, training=self.training)
-        xY2 = self.convY2(xY1, edge_index)
-        return xB2, xY2
+class impactDetect(torch.nn.Module):
+    def __init__(self, in_dim, h_dim, out_dim=1):
+        super(impactDetect, self).__init__()
+        self.convZ1 = GCNConv(in_dim, h_dim)
+        self.convZ2 = GCNConv(h_dim, h_dim)
 
-## TODO: Impact estimation module
+        self.yNet = torch.nn.Sequential(torch.nn.Linear(h_dim, out_dim), torch.nn.ReLU())
+        self.propenNet = torch.nn.Sequential(torch.nn.Linear(h_dim, out_dim), torch.nn.ReLU())
+        self.balanceNet = torch.nn.Sequential(torch.nn.Linear(h_dim, out_dim), torch.nn.ReLU())
+
+    def forward(self, x, edge_index, fake_x, fake_edge_index):
+        # generate node embedding
+        xZ1, xfZ1 = F.relu(self.convZ1(x, edge_index)), F.relu(self.convZ1(fake_x, fake_edge_index))
+        xZ1, xfZ1 = F.dropout(xZ1, p=0.5, training=self.training), F.dropout(xfZ1, p=0.5, training=self.training)
+        xZ2, xfZ2 = self.convZ2(xZ1, edge_index), self.convZ2(xfZ1, fake_edge_index)
+
+        # predict outcome
+        yi = self.yNet(xZ2)
+        # judge the node is from factual & counterfactual graph
+        fprob, fprob_f = self.propenNet(xZ2), self.propenNet(xfZ2)
+        # judge the node is treated or controled
+        treat_prob = self.balanceNet(xZ2)
+
+        return yi, fact_prob, fact_prob_f, treat_prob
+
+
 
 def generate_counterfactual_edge(N, var_edge_index, inv_edge_index):
     '''
@@ -94,7 +112,8 @@ target_var = torch.LongTensor(np.concatenate([bot_label[:,np.newaxis], outcome[:
 botData = Data(x=x.unsqueeze(-1), edge_index=edge_index.t().contiguous(), y=target_var, train_mask=train_mask, test_mask=test_mask)
 
 model1 = MaskEncoder(in_dim=1, h_dim=16, out_dim=16)
-model2 = botDetect(in_dim=1, h_dim1=16, h_dim2=16, out_dim1=1, out_dim2=1)
+model2 = botDetect(in_dim=1, h_dim=16, out_dim=1)
+model3 = impactDetect()
 optimizer = torch.optim.Adam(model2.parameters(), lr=0.01)
 
 def train():
@@ -113,14 +132,24 @@ def train():
         botData_fake_fact = Data(x=x.unsqueeze(-1), edge_index=fake_fact_graph.t().contiguous(), y=target_var, train_mask=train_mask,
                        test_mask=test_mask)
 
-        out_b, out_y = model2(botData.x, botData.edge_index)
-        out_b_fake_fact, _ = model2(botData_fake_fact.x, botData_fake_fact.edge_index)
-
-        target_b, target_y = botData.y[:,0][botData.train_mask], botData.y[:,1][botData.train_mask]
+        # detect bot
+        out_b = model2(botData.x, botData.edge_index)
+        out_b_fake_fact = model2(botData_fake_env.x, botData_fake_env.edge_index)
+        target_b = botData.y[:,0][botData.train_mask]
+        target_b_fake = botData_fake_env.y[:,0][botData.train_mask]
 
         # 最大化所有环境下bot识别的准确度
         loss_b = anomaly_loss(target_b, out_b[botData.train_mask])
-        loss_b_fake = anomaly_loss(out_b_fake_fact.y[:,0][botData.train_mask], out_b_fake_fact[botData.train_mask])
+        loss_b_fake = anomaly_loss(target_b_fake, out_b_fake_fact[botData.train_mask])
+
+
+
+        # assess bot impact
+        out_y, fact_prob, fact_prob_f, treat_prob = model3(botData.x, botData.edge_index,
+                                              botData_fake_fact.x, botData_fake_fact.edge_index)
+        target_y = botData.y[:, 1][botData.train_mask]
+        target_judgefact = torch.cat([torch.ones(len(fact_prob)),torch.zeros(len(fact_prob_f))], dim=-1)
+        out_judgefact = torch.cat([fact_prob, fact_prob_f])
 
         loss_y = anomaly_loss(target_y, out_y[botData.train_mask])
 
@@ -130,7 +159,7 @@ def train():
 
         if epoch%10 == 0:
             model2.eval()
-            out_b, out_y = model2(botData.x, botData.edge_index)
+            out_b = model2(botData.x, botData.edge_index)
             target_b, target_y = botData.y[:, 0][botData.train_mask], botData.y[:, 1][botData.train_mask]
             tc_label = botData.y[:, 2][botData.train_mask]
 
@@ -164,7 +193,7 @@ def train():
 @torch.no_grad()
 def test():
     model2.eval()
-    out_b, out_y = model2(botData.x, botData.edge_index)
+    out_b = model2(botData.x, botData.edge_index)
 
     # bot detection
     threshold = torch.quantile(out_b, 0.96, dim=None, keepdim=False, interpolation='higher')
