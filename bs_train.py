3811c43bfbd4ad0feae2cd063c7219ae4b79f407
Ziyue Wu <670122025@qq.com>
2024-03-21 16:50:23 +0800

    update code

--- bs_train.py
+++ bs_train.py
@@ -19,16 +19,19 @@ parser = argparse.ArgumentParser('Benchmark')
 
 # data parameters
 parser.add_argument('--type', type=str, help='data used', default='random')
-parser.add_argument('--effect_true', type=float, help='ground-truth effect', default=0) # synthetic: -1, empirical: 0
+parser.add_argument('--effect_true', type=float, help='ground-truth effect', default=-1) # synthetic: -1, empirical: 0
 # model parameters
 # ignite, gial, cne-, dd
-parser.add_argument('--model', type=str, help='model test', default='ignite')
+parser.add_argument('--model', type=str, help='model test', default='gial')
 # training parameters
-parser.add_argument('--epoch', type=int, help='num epochs', default=300)
+parser.add_argument('--epoch', type=int, help='num epochs', default=400)
 parser.add_argument('--gpu', type=int, help='gpu', default=0)
 # saving embedding
-parser.add_argument('--save', type=bool, help='whether save emb', default=False)
-parser.add_argument('--save_epoch', type=int, help='saving emb epoch', default=150)
+parser.add_argument('--save_train', type=bool, help='save training result', default=False)
+# syn: ignite: 120, gial: 100, dd: 200, cne-: 350
+parser.add_argument('--rep_epoch', type=int, help='save epoch result', default=200) # syn: 350, emp: 500
+
+
 
 try:
     args = parser.parse_args()
@@ -47,28 +50,27 @@ def set_seed(seed):
         torch.cuda.manual_seed_all(seed)
 
 
-
-
-
 def evaluate_metric(pred_0, pred_1, pred_c1, pred_c0):
     tau_pred = torch.cat([pred_c1, pred_1], dim=0) - torch.cat([pred_0, pred_c0], dim=0)
-    print("treat ave: {:.4f}".format(torch.mean(torch.cat([pred_1, pred_c1], dim=0)).item()))
-    print("control ave: {:.4f}".format(torch.mean(torch.cat([pred_0, pred_c0], dim=0)).item()))
+    ave_treat = torch.mean(torch.cat([pred_1, pred_c1], dim=0)).item()
+    ave_control = torch.mean(torch.cat([pred_0, pred_c0], dim=0)).item()
+    print("treat ave: {:.4f}".format(ave_treat))
+    print("control ave: {:.4f}".format(ave_control))
     print("--------------------------------")
     tau_true = torch.ones(tau_pred.shape).to(device) * args.effect_true
     ePEHE = torch.sqrt(torch.mean(torch.square(tau_pred-tau_true)))
     eATE = torch.abs(torch.mean(tau_pred) - torch.mean(tau_true))
     Treat_eff = torch.mean(tau_pred)
-    return eATE, ePEHE, Treat_eff
+    return eATE, ePEHE, Treat_eff, ave_treat, ave_control
 
-def load_data(dt='train'):
+def load_data(data_id):
     if args.type in ['random', 'randomu', 'highbc', 'highcc', 'lowdu', 'highdu']:
         # load train data
-        edge_index = torch.LongTensor(np.load('Dataset/synthetic/'+args.type+'/'+dt+'_edge.npy'))    # (num_edge, 2)
-        bot_label = np.load('Dataset/synthetic/'+args.type+'/'+dt+'_bot_label.npy')
-        treat_indicator = np.load('Dataset/synthetic/'+args.type+'/'+dt+'_T_label.npy')
-        outcome = np.load('Dataset/synthetic/'+args.type+'/'+dt+'_y.npy')
-        prop_label = np.load('Dataset/synthetic/'+args.type+'/'+dt+'_prop_label.npy')
+        edge_index = torch.LongTensor(np.load('Dataset/synthetic/'+args.type+'/'+str(data_id)+'_edge.npy'))    # (num_edge, 2)
+        bot_label = np.load('Dataset/synthetic/'+args.type+'/'+str(data_id)+'_bot_label.npy')
+        treat_indicator = np.load('Dataset/synthetic/'+args.type+'/'+str(data_id)+'_T_label.npy')
+        outcome = np.load('Dataset/synthetic/'+args.type+'/'+str(data_id)+'_y.npy')
+        prop_label = np.load('Dataset/synthetic/'+args.type+'/'+str(data_id)+'_prop_label.npy')
     elif args.type in ['t1_pos', 't2_pos', 't3_pos', 't1_neg', 't2_neg', 't3_neg']:
         # load train data
         edge_index = torch.LongTensor(np.load('Dataset/twi22/'+args.type[:2]+'/'+args.type+'_edge.npy'))    # (num_edge, 2)
@@ -89,27 +91,27 @@ def load_data(dt='train'):
 
 
 
-def main():
-    botData_f, N_train, prop_label_train = load_data('train')
+def train():
+    botData_f, N_train, prop_label_train = load_data(data_id)
     print("Finish loading data.")
     #treat_idx, control_idx = torch.where(botData_f.y[:, 2] == -1)[0], torch.where(botData_f.y[:, 2] == 1)[0]
     treat_idx, control_idx = torch.nonzero(botData_f.y[:, 2] == -1).squeeze(), torch.nonzero(botData_f.y[:, 2] == 1).squeeze()
 
     if args.model == 'ignite':
-        model_g = IgniteGenerator(in_dim=1, h_dim=16, out_dim=1).to(device)
-        model_d = IgniteDiscriminator(h_dim=16).to(device)
+        model_g = IgniteGenerator(in_dim=1, h_dim=32, out_dim=1).to(device)
+        model_d = IgniteDiscriminator(h_dim=32).to(device)
         optimizer_g = torch.optim.Adam(model_g.parameters(), lr=0.001)
         optimizer_d = torch.optim.Adam(model_d.parameters(), lr=0.001)
     elif args.model == 'gial':
-        model_g = GialGenerator(in_dim=1, h_dim=16, out_dim=1).to(device)
-        model_d = GialDiscriminator(h_dim=16).to(device)
+        model_g = GialGenerator(in_dim=1, h_dim=32, out_dim=1).to(device)
+        model_d = GialDiscriminator(h_dim=32).to(device)
         optimizer_g = torch.optim.Adam(model_g.parameters(), lr=0.001)
         optimizer_d = torch.optim.Adam(model_d.parameters(), lr=0.001)
     elif args.model == 'cne-':
-        model = CNE_Minus(in_dim=1, h_dim=16, out_dim=1).to(device)
+        model = CNE_Minus(in_dim=1, h_dim=32, out_dim=1).to(device)
         optimizer = torch.optim.Adam([{'params': model.parameters(), 'lr': 0.001}])
     elif args.model == 'dd':
-        model = DD(in_dim=1, h_dim=16, out_dim=1).to(device)
+        model = DD(in_dim=1, h_dim=32, out_dim=1).to(device)
         optimizer = torch.optim.Adam([{'params': model.parameters(), 'lr': 0.001}])
 
     # train
@@ -138,7 +140,7 @@ def main():
             loss_d.backward()
             optimizer_d.step()
             # Loss function
-            #print("{:.4f} {:.4f}".format(loss_y.item(), loss_cb.item()))
+            # print("{:.4f} {:.4f}".format(loss_y.item(), loss_cb.item()))
 
         elif args.model == 'gial':
             model_g.train()
@@ -171,7 +173,7 @@ def main():
             out_mi = torch.cat([D, Df], dim=0)
             loss_jf = torch.nn.CrossEntropyLoss()(out_judgefact, target_judgefact.long())
             loss_m = torch.nn.BCELoss()(out_mi, target_mi.float())
-            loss_d = - loss_m * 1 + loss_jf * 1
+            loss_d = - loss_m * 1 + loss_jf * 100
             loss_d.backward()
             optimizer_d.step()
             #print("{:.4f} {:.4f} {:.4f}".format(loss_y.item(), loss_jf.item(), loss_m.item()))
@@ -187,7 +189,7 @@ def main():
             out_judgetreat = torch.cat([treat_prob[treat_idx], treat_prob[control_idx]], dim=0)
             loss_y = F.mse_loss(out_y.float(), target_y.float())
             loss_judgetreat = torch.nn.CrossEntropyLoss()(out_judgetreat, target_judgetreat.long())
-            loss = loss_y * 1 + loss_judgetreat * 1
+            loss = loss_y * 1 + loss_judgetreat * 1000
             loss.backward()
             optimizer.step()
             #print("{:.4f} {:.4f}".format(loss_y.item(), loss_judgetreat.item()))
@@ -201,10 +203,11 @@ def main():
             loss_y = F.mse_loss(out_y.float(), target_y.float())
             samples_loss = SamplesLoss(loss="sinkhorn", p=2, blur=0.005, backend="tensorized") ## TODO: reduce the computational cost
             imbalance_loss = samples_loss(z[treat_idx], z[control_idx])
-            loss = loss_y * 1 + imbalance_loss * 1
+            loss = loss_y * 1 + imbalance_loss * 10000
             loss.backward()
             optimizer.step()
             #print("{:.4f} {:.4f}".format(loss_y.item(), imbalance_loss.item()))
+
 
         if epoch%10 == 0:
             if args.model == 'ignite':
@@ -217,34 +220,56 @@ def main():
                 out_y1, out_yc0, out_y0, out_yc1, z, zf = model_g(botData_f.x, botData_f.edge_index, shuffled_x, treat_idx, control_idx)
             elif args.model == 'cne-':
                 model.eval()
-                out_y1, out_yc0, out_y0, out_yc1, treat_prob = model(botData_f.x, botData_f.edge_index, treat_idx, control_idx)
+                out_y1, out_yc0, out_y0, out_yc1, treat_prob, z = model(botData_f.x, botData_f.edge_index, treat_idx, control_idx)
             elif args.model == 'dd':
                 model.eval()
                 out_y1, out_yc0, out_y0, out_yc1, z = model(botData_f.x, botData_f.edge_index, treat_idx, control_idx)
 
             # treatment effect prediction result
-            eATE_test, ePEHE_test, treat_eff  = evaluate_metric(out_y0, out_y1, out_yc1, out_yc0)
-            print("Epoch: " + str(epoch))
-            print('eATE: {:.4f}'.format(eATE_test.detach().cpu().numpy()),
-                  'ePEHE: {:.4f}'.format(ePEHE_test.detach().cpu().numpy()),
-                  'Effect: {:.4f}'.format(treat_eff.detach().cpu().numpy()))
-            print("================================")
-            # save embedding
-            if epoch == args.save_epoch:
-                Z_emb, treat_lb, y_tar = z.detach().cpu().numpy(), botData_f.y[:, 2].detach().cpu().numpy(), botData_f.y[:, 1].detach().cpu().numpy()
-                if args.save:
-                    savedt_path = 'result/save_emb/'+args.type+'/'
-                    if not os.path.isdir(savedt_path):
-                        os.makedirs(savedt_path)
-                    np.save(savedt_path+args.model+str(seed)+'.npy', np.concatenate([Z_emb,treat_lb[:,np.newaxis], y_tar[:,np.newaxis]], axis=-1)) # emb_H + treat (dim: z+1)
+            eATE_test, ePEHE_test, treat_eff, ave_treat, ave_control  = evaluate_metric(out_y0, out_y1, out_yc1, out_yc0)
+            print("Epoch: " + str(epoch), "Data: ", args.type)
 
+            eATE_test = eATE_test.detach().cpu().numpy()
+            ePEHE_test = ePEHE_test.detach().cpu().numpy()
+            treat_eff = treat_eff.detach().cpu().numpy()
+
+            print('eATE: {:.4f}'.format(eATE_test),
+                  'ePEHE: {:.4f}'.format(ePEHE_test),
+                  'Effect: {:.4f}'.format(treat_eff))
+            print("================================")
+
+            # check diff data
+            if epoch == args.rep_epoch:
+                eATE_R, ePEHE_R, Mean1, Mean0 = eATE_test, ePEHE_test, ave_treat, ave_control
+                res_dt['Data_id'].append(data_id)
+                res_dt['aveT'].append(Mean1)
+                res_dt['aveC'].append(Mean0)
+                res_dt['eATE'].append(eATE_R)
+                res_dt['ePEHE'].append(ePEHE_R)
+                print("Finish training: ", data_id)
+                print('eATE: {:.4f}'.format(eATE_R),
+                      'ePEHE: {:.4f}'.format(ePEHE_R),
+                      'mean1: {:.4f}'.format(Mean1),
+                      'mean0: {:.4f}'.format(Mean0))
+                print("================================")
 
 
 
 if __name__ == "__main__":
-    for seed in range(101, 111):
-        set_seed(seed)
-        res = {'Epoch': [], 'eATE': [], 'ePEHE': [], 'rjf': [], 'lcff': [], 'ly': []}
-        main()
-        res = pd.DataFrame(res)
-        res.to_csv('result/'+args.model+'_'+str(seed)+'.csv', index=False)
+    # check diff seed
+    # for seed in range(101, 102):
+    #     data_id = 0
+    #     set_seed(seed)
+    #     res = {'Epoch': [], 'aveT': [], 'aveC': [], 'eATE': [], 'ePEHE': [], 'rjf': [], 'lcff': [], 'ly': []}
+    #     train()
+    #     if args.save_train:
+    #         res = pd.DataFrame(res)
+    #         res.to_csv('result/'+args.model+'_'+ args.type + str(seed) + '.csv', index=False)
+    #
+    # check diff data (for synthetic data)
+    set_seed(101)
+    res_dt = {'Data_id': [], 'aveT': [], 'aveC': [], 'eATE': [], 'ePEHE': []}
+    for data_id in range(100): # 100
+        train()
+    res_dt = pd.DataFrame(res_dt)
+    res_dt.to_csv('result/'+args.model+'_'+args.type+'_all.csv', index=False)
