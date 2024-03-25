addb19a15f9d0c53c83b9984b9ec7d82240d6088
Ziyue Wu <670122025@qq.com>
2024-03-25 16:15:10 +0800

    update code

--- util.py
+++ util.py
@@ -14,15 +14,15 @@ def match_node(fake_fact_graph, bot_label, prop_label, treat_idx, control_idx):
     for id in treat_idx.tolist():
         if id not in friend_dict:
             continue
-        friend = friend_dict[id]  # id's friend
-        if bot_label[friend].sum()>0 and prop_label[friend].sum()==0:
+        friend = friend_dict[id]  # id's friend 
+        if prop_label[friend].sum()>0 and bot_label[friend].sum()==0: # the node is in the control group in Gcf
             treat_idx_ok.append(id)
 
     for id in control_idx.tolist():
         if id not in friend_dict:
             continue
         friend = friend_dict[id]  # id's friend
-        if prop_label[friend].sum()>0 and bot_label[friend].sum()==0:
+        if bot_label[friend].sum()>0 and prop_label[friend].sum()==0: # the node is in the treated group in Gcf
             control_idx_ok.append(id)
 
     return torch.LongTensor(treat_idx_ok), torch.LongTensor(control_idx_ok)
